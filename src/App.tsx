1f7ad2724fd0edc4972e13794c9a94435959d664
PanidharGUdupa <panidharu@gmail.com>
2026-04-18 00:56:11 +0530

    some deploy setup

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename="/panidhargudupa-portfolio/">
         <ScrollToTop />
         <AnimatedRoutes />
       </BrowserRouter>
